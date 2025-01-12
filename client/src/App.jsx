d41674b7e5a219048d8f05e3817b04189729e890
Pooja675 <poojasinha675@gmail.com>
2025-01-12 14:23:58 +0530

    Installed Reactjs & Tailwind CSS

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+     <h1 className="flex justify-center my-10 text-green-700 font-bold">Hello MERN Blog.....</h1>
+    </>
+  )
+}
+
+export default App
