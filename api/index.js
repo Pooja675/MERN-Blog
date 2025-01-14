7cfceb4aecb6dbb82f1830f8a38597e31eb5a327
Pooja675 <poojasinha675@gmail.com>
2025-01-14 13:18:59 +0530

    add functionality to the sign up page

--- api/index.js
+++ api/index.js
@@ -2,9 +2,17 @@ const express = require("express")
 const connectDB = require("../config/database")
 const userRouter = require("./routes/user.route");
 const authRouter = require("./routes/auth.route");
+const cors = require("cors")
 const app = express()
 
 app.use(express.json())
+
+// app.use(cors({
+
+//     origin:"http://localhost:5173",
+//     credentials: true,
+//   }))
+  
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,11 +1,54 @@
-import { Button, Label, TextInput } from "flowbite-react";
-import React from "react";
-import { Link } from "react-router-dom";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constant";
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessge] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate()
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Check if all fields are provided
+    if (!formData.userName || !formData.emailId || !formData.password) {
+      return setErrorMessge("All fields are required.");
+    }
+
+    try {
+      setLoading(true);
+      setErrorMessge(null);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+      if (data.success === false) {
+        return setErrorMessge(data.message);
+      }
+
+      setLoading(false);
+      if(res.ok) {
+        navigate('/sign-in')
+      }
+    } catch (error) {
+      setErrorMessge(error.message);
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="min-h-screen mt-20">
-      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-8">
+      <div className="flex p-3 max-w-4xl mx-auto flex-col md:flex-row md:items-center gap-8">
         {/* left */}
         <div className="flex-1">
           <Link to="/" className="text-4xl font-bold dark:text-white">
@@ -25,34 +68,61 @@ const SignUp = () => {
         </div>
         {/* right */}
         <div className="flex-1">
-          <form className="flex flex-col gap-4">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <div>
               <Label value="Your username" />
 
               <TextInput
-                id="username"
+                id="userName"
                 type="text"
                 placeholder="Username"
                 required
+                onChange={handleChange}
               />
             </div>
             <div>
               <Label value="Your email" />
 
-              <TextInput id="email" type="text" placeholder="Email" />
+              <TextInput
+                id="emailId"
+                type="email"
+                placeholder="Email"
+                onChange={handleChange}
+              />
             </div>
             <div>
               <Label value="Your password" />
 
-              <TextInput id="password" type="password" placeholder="Password" />
+              <TextInput
+                id="password"
+                type="password"
+                placeholder="Password"
+                onChange={handleChange}
+              />
             </div>
-            
-            <Button type="submit" gradientDuoTone="purpleToBlue">Submit</Button>
+
+            <Button type="submit" gradientDuoTone="purpleToBlue" disabled={loading}>
+              {loading ? (
+                <>
+                <Spinner size="sm"/>
+                <span className="pl-3">Loading...</span>
+                </>
+              ) : (
+                'Sign up'
+              )}
+            </Button>
           </form>
           <div className="flex text-sm gap-2 mt-5">
             <span>Have an account?</span>
-            <Link to="/sign-in" className="text-blue-500">Sign In</Link>
+            <Link to="/sign-in" className="text-blue-500">
+              Sign In
+            </Link>
           </div>
+          {errorMessage && (
+            <Alert className="mt-5" color="failure">
+              {errorMessage}
+            </Alert>
+          )}
         </div>
       </div>
     </div>
