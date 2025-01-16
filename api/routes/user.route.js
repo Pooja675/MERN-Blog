9bae38188c33b4833a8a3298de051ce01d5d903b
Pooja675 <poojasinha675@gmail.com>
2025-01-16 17:28:40 +0530

    add delete user api

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,11 @@
 const express = require ("express");
-const { test, updateUser } = require("../controllers/user.controller");
+const { test, updateUser, deleteUser } = require("../controllers/user.controller");
 const verifyUser = require("../utils/verifyUser");
 
 const userRouter = express.Router()
 
 userRouter.get("/test", test)
 userRouter.put("/update/:userId",verifyUser, updateUser)
+userRouter.delete("/delete/:userId",verifyUser, deleteUser)
 
 module.exports = userRouter;
