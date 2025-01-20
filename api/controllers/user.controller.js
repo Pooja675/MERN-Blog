a6d19fe9263208341bd54830bd9cb942bd896b58
Pooja675 <poojasinha675@gmail.com>
2025-01-20 20:47:28 +0530

    create get user api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -25,7 +25,7 @@ const updateUser = async (req, res, next) => {
         errorHandler(400, "UserName must be between 7 and 20 characters")
       );
     }
-    if (req.body.userName.includes(' ')) {
+    if (req.body.userName.includes(" ")) {
       return next(errorHandler(400, "UserName cannot contain space"));
     }
     if (req.body.userName !== req.body.userName.toLowerCase()) {
@@ -49,41 +49,88 @@ const updateUser = async (req, res, next) => {
           profilePicture: req.body.profilePicture,
         },
       },
-      { new: true });
-      const {password, ...rest} = updatedUser._doc;
-      res.status(200).json(rest);
+      { new: true }
+    );
+    const { password, ...rest } = updatedUser._doc;
+    res.status(200).json(rest);
   } catch (error) {
     next(error);
   }
 };
 
 const deleteUser = async (req, res, next) => {
-
   if (req.user._id !== req.params.userId) {
     return next(errorHandler(403, "You are not allowed to delete this user"));
   }
 
   try {
-    await User.findByIdAndDelete(req.params.userId)
-    res.status(200).json("User has been deleted!!")
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(200).json("User has been deleted!!");
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
 const signout = async (req, res, next) => {
-      try {
+  try {
+    res
+      .clearCookie("access_Token")
+      .status(200)
+      .json("User has been signed out");
+  } catch (error) {
+    next(error);
+  }
+};
 
-        res.clearCookie('access_Token').status(200).json("User has been signed out")
-        
-      } catch (error) {
-        next(error)
-      }
-}
+const getusers = async (req, res, next) => {
+
+  if (!req.user.isAdmin) {
+    return next(errorHandler(403, "You are not allowed to see all users"));
+  }
+
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.sort === "asc" ? 1 : -1;
+
+    const users = await User.find()
+      .sort({ createdAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+      const userWithoutPassword = users.map((user) => { //getting array of users without password 
+        const {password, ...rest} = user._doc;
+        return rest;
+      })
+
+      const totalUsers = await User.countDocuments();
+
+      const now = new Date();
+
+      const oneMonthAgo = new Date(
+        now.getFullYear(),
+        now.getMonth() - 1,
+        now.getDate(),
+      )
+
+      const lastMonthUsers = await User.countDocuments({
+        createdAt: {$gte: oneMonthAgo}
+      })
+
+      res.status(200).json({
+        users:userWithoutPassword,
+        totalUsers,
+        lastMonthUsers,
+      })
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports = {
   test,
   updateUser,
-  deleteUser, 
+  deleteUser,
   signout,
+  getusers,
 };
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 const express = require ("express");
-const { test, updateUser, deleteUser, signout } = require("../controllers/user.controller");
+const { test, updateUser, deleteUser, signout, getusers } = require("../controllers/user.controller");
 const verifyUser = require("../utils/verifyUser");
 
 const userRouter = express.Router()
@@ -8,5 +8,6 @@ userRouter.get("/test", test)
 userRouter.put("/update/:userId",verifyUser, updateUser)
 userRouter.delete("/delete/:userId",verifyUser, deleteUser)
 userRouter.post("/signout", signout)
+userRouter.get("/getusers",verifyUser, getusers)
 
 module.exports = userRouter;
