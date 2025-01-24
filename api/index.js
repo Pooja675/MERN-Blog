1f1f7e70ed81c78721242693d9819796f18a120a
Pooja675 <poojasinha675@gmail.com>
2025-01-24 18:10:18 +0530

    updated

--- api/index.js
+++ api/index.js
@@ -34,10 +34,10 @@ app.use("/api/auth", authRouter);
 app.use("/api/post", postRouter);
 app.use("/api/comment", commentRouter);
 
-app.use(express.static(path.resolve(__dirname, "client", "dist")));
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
 
 
