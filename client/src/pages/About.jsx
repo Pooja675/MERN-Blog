0fa685a992dc6e7e84ee003f888e9dccc57d45fb
Pooja675 <poojasinha675@gmail.com>
2025-01-24 10:43:38 +0530

    Complete about and project pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,10 +2,28 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <div>
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-2 text-center'>
         <div>
-          <h1></h1>
+          <h1 className='text-3xl font font-semibold text-center my-5'>WebDev Chronicles</h1>
+          <div className='text-md text-gray-600 flex flex-col gap-6'> 
+            <p> 
+            The WebDeb Chronicles is a thoughtfully curated blog that explores the fascinating world of web development. 
+            Aimed at developers of all skill levels, it serves as a go-to resource for learning the latest trends, mastering 
+            foundational concepts, and staying informed about emerging technologies in the ever-evolving tech landscape.
+            </p>
+            <p>
+            What sets The WebDeb Chronicles apart is its commitment to real-world problem-solving. The blog frequently includes 
+            case studies, troubleshooting guides, and hands-on projects, empowering readers to apply their knowledge in meaningful 
+            ways. Whether you’re debugging a tricky API integration or optimizing your app’s performance, you’ll find actionable 
+            solutions here.
+            </p>
+            <p>
+            With a strong focus on community and growth, The WebDeb Chronicles isn’t just a blog—it’s a companion on your journey to 
+            becoming a proficient and confident web developer. Dive in to level up your skills and stay ahead in the competitive digital 
+            landscape!
+            </p>
+          </div>
         </div>
       </div>
     </div>
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import CallToAction from "../components/CallToAction";
 
 const Projects = () => {
   return (
-    <div>Projects</div>
-  )
-}
+    <div className="min-h-screen max-w-2xl mx-auto flex flex-col items-center justify-center gap-6 p-6">
+      <h1 className="text-3xl font-semibold">Projects</h1>
+      <p className="text-lg text-gray-600 text-justify leading-relaxed tracking-wide">
+        At WebDev Chronicles, we bring you curated MERN project ideas,
+        step-by-step guides, and best practices to help you level up your
+        portfolio. Whether you're debugging your first CRUD app or deploying a
+        production-ready app, these projects are designed to enhance your skills
+        and boost your confidence as a full-stack developer.
+      </p>
+      <CallToAction />
+    </div>
+  );
+};
 
-export default Projects
+export default Projects;
