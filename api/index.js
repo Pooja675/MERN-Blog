d5dba8fb0ec72f0d2fa0f52371ae739827fd4676
Pooja675 <poojasinha675@gmail.com>
2025-01-24 16:10:06 +0530

    updated

--- api/index.js
+++ api/index.js
@@ -1,47 +1,49 @@
-const express = require("express")
-const connectDB = require("./config/database")
+const express = require("express");
+const monggose = require("mongoose");
+//const connectDB = require("./config/database")
 const userRouter = require("./routes/user.route");
 const authRouter = require("./routes/auth.route");
 const cookieParser = require("cookie-parser");
 const postRouter = require("./routes/post.route");
 const commentRouter = require("./routes/comment.route");
-const path = require("path")
-const app = express()
+const dotenv = require("dotenv")
 
-app.use(express.json())
-app.use(cookieParser())  
+const path = require("path");
+const app = express();
+dotenv.config()
 
+app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/post", postRouter);
-app.use("/api/comment", commentRouter)
+app.use("/api/comment", commentRouter);
 
-app.use(express.static(path.join(__dirname, '/client/dist')))
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
-app.get('*', (req,res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
-})
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
-connectDB()
-    .then(() => {
-        console.log("Database connection established..")
-        app.listen("5555", () => {
-            console.log("Server is running on port 5555!!")
-        })
-    })
-    .catch(() => {
-        console.log("Database cannot be connected....")
-    })
+monggose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Database connection established..");
+    app.listen("5555", () => {
+      console.log("Server is running on port 5555!!");
+    });
+  })
+  .catch(() => {
+    console.log("Database cannot be connected....");
+  });
 
-    
-   app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || "Internal Server Error";
-    res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message
-    })
-   }) 
-    
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -5,7 +5,7 @@ const Projects = () => {
   return (
     <div className="min-h-screen max-w-2xl mx-auto flex flex-col items-center justify-center gap-6 p-6">
       <h1 className="text-3xl font-semibold">Projects</h1>
-      <p className="text-lg text-gray-600 text-justify leading-relaxed tracking-wide">
+      <p className="text-lg text-teal-600 text-justify leading-relaxed tracking-wide">
         At WebDev Chronicles, we bring you curated MERN project ideas,
         step-by-step guides, and best practices to help you level up your
         portfolio. Whether you're debugging your first CRUD app or deploying a
